0553f1e3a8bc710bf348ca7ede281313b3364401
cchanal <christophechanal@live.fr>
2016-01-15 10:39:49 +0100

    List existant projects on homepage

--- documentify/documentify/Controllers/HomeController.cs
+++ documentify/documentify/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using documentify.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,12 @@ namespace documentify.Controllers
 {
     public class HomeController : Controller
     {
+        private documentifyDataEntities db = new documentifyDataEntities();
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<projet> projet = new List<projet>();
+            projet = db.projets.ToList();
+            return View(projet);
         }
 
         public ActionResult About()
