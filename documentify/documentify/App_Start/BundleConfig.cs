62755342d581bcd39cbf49722159b967da27c2d1
cchanal <christophechanal@live.fr>
2016-01-15 09:45:50 +0100

    change homepage and add a few style

--- documentify/documentify/App_Start/BundleConfig.cs
+++ documentify/documentify/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace documentify
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/style.css"));
 
             // Définissez EnableOptimizations sur False pour le débogage. Pour plus d'informations,
             // visitez http://go.microsoft.com/fwlink/?LinkId=301862
