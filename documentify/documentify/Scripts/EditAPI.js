a62dfb7c241e01d4cbd8ffe2284fb6ef712e9306
cchanal <christophechanal@live.fr>
2016-01-20 13:58:33 +0100

    Add edition on page

---
+++ documentify/documentify/Scripts/EditAPI.js
@@ -0,0 +1,13 @@
+﻿var EditAPI = (function () {
+    function init() {
+        $('.jumbotron, article').froalaEditor({
+            toolbarInline: true,
+            charCounterCount: false,
+            toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', 'color', 'emoticons', '-', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'indent', 'outdent', '-', 'insertImage', 'insertLink', 'insertFile', 'insertVideo', 'undo', 'redo']
+        })
+    }
+
+    return{
+        init: init
+    }
+})();
