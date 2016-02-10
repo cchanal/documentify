148e718f5147f743a73b004af26f6c33b8c8494b
cchanal <christophechanal@live.fr>
2016-02-10 10:34:11 +0100

    add capability to add section

--- documentify/documentify/Scripts/EditAPI.js
+++ documentify/documentify/Scripts/EditAPI.js
@@ -3,7 +3,8 @@
         $('.jumbotron, article').froalaEditor({
             toolbarInline: true,
             charCounterCount: false,
-            toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', 'color', 'emoticons', '-', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'indent', 'outdent', '-', 'insertImage', 'insertLink', 'insertFile', 'insertVideo', 'undo', 'redo']
+            language: 'fr',
+            toolbarButtons: ['paragraphFormat', 'bold', 'italic', 'underline', 'strikeThrough', 'align', '-', 'formatOL', 'formatUL', '|', 'insertLink', 'insertTable', 'undo', 'redo']
         })
     }
 
---
+++ documentify/documentify/Scripts/PageAPI.js
@@ -0,0 +1,15 @@
+﻿var PageAPI = (function () {
+    function init() {
+        $(".add-section").on('click', function () {
+            var lastOrderSectionNumber = $("section article").last().attr('id');
+            lastOrderSectionNumber++;
+            console.log("i");
+            $(this).before("<article id='" + lastOrderSectionNumber + "'><h1 id='test'>Titre</h1><p>Contenu</p></article>");
+            EditAPI.init();
+        })
+    }
+
+    return {
+        init: init
+    }
+})();
